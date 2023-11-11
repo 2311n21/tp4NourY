60e00b9177b77b727334acaec6b82111695ebca5
ny19 <nour.yatefti@gmail.com>
2023-11-11 23:37:20 +0100

    Modifier nom

---
+++ src/main/java/com/emsi/tpbanquenoury/jsf/Modifier.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.emsi.tpbanquenoury.jsf;
+
+import com.emsi.tpbanquenoury.entity.CompteBancaire;
+import com.emsi.tpbanquenoury.service.GestionnaireCompte;
+import com.emsi.tpbanquenoury.util.Util;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import java.io.Serializable;
+
+/**
+ *
+ * @author mac
+ */
+@Named(value = "modifier")
+@ViewScoped
+public class Modifier implements Serializable {
+
+    private Long id;
+    private CompteBancaire compte;
+    private String nom;
+
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+
+    /**
+     * Creates a new instance of Modifier
+     */
+    public Modifier() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public CompteBancaire getCompte() {
+        return compte;
+    }
+
+    public void loadCompte() {
+        compte = gestionnaireCompte.getCompte(id);
+    }
+
+    public String enregistrerModification() {
+        Util.addFlashInfoMessage("Nom " + compte.getNom() + " changé en " + nom);
+        compte.setNom(nom);
+        gestionnaireCompte.update(compte);
+        return "listeComptes?faces-redirect=true";
+    }
+
+}
